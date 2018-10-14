c5fee25d6b574d6154ab8f8510f77eccfa706e87
faddi <faddi.idris@gmail.com>
2018-10-14 10:06:29 +0200

    added other solution

--- 1/main.py
+++ 1/main.py
@@ -70,7 +70,13 @@ import torch.optim as optim
 import torch.nn.functional as F
 import torchvision.transforms as T
 
+random.seed(0)
+np.random.seed(0)
+torch.cuda.manual_seed(0)
+torch.manual_seed(0)
+
 env = gym.make('CartPole-v0').unwrapped
+env.seed(0)
 
 # set up matplotlib
 is_ipython = 'inline' in matplotlib.get_backend()
@@ -81,6 +87,10 @@ plt.ion()
 
 # if gpu is to be used
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+print("cuda") if torch.cuda.is_available() else print("cpu")
+ep = 0
 
 ######################################################################
 # Replay Memory
@@ -198,22 +208,54 @@ class ReplayMemory(object):
 #
 
 
+# class DQN(nn.Module):
+#     def __init__(self):
+#         super(DQN, self).__init__()
+#         self.conv1 = nn.Conv2d(1, 16, kernel_size=5, stride=2)
+#         self.bn1 = nn.BatchNorm2d(16)
+#         self.conv2 = nn.Conv2d(16, 32, kernel_size=5, stride=2)
+#         self.bn2 = nn.BatchNorm2d(32)
+#         self.conv3 = nn.Conv2d(32, 32, kernel_size=5, stride=2)
+#         self.bn3 = nn.BatchNorm2d(32)
+#         self.head = nn.Linear(448, 2)
+#
+#     def forward(self, x):
+#         x = F.relu(self.bn1(self.conv1(x)))
+#         x = F.relu(self.bn2(self.conv2(x)))
+#         x = F.relu(self.bn3(self.conv3(x)))
+#         return self.head(x.view(x.size(0), -1))
+
 class DQN(nn.Module):
     def __init__(self):
         super(DQN, self).__init__()
-        self.conv1 = nn.Conv2d(1, 16, kernel_size=5, stride=2)
-        self.bn1 = nn.BatchNorm2d(16)
-        self.conv2 = nn.Conv2d(16, 32, kernel_size=5, stride=2)
-        self.bn2 = nn.BatchNorm2d(32)
-        self.conv3 = nn.Conv2d(32, 32, kernel_size=5, stride=2)
-        self.bn3 = nn.BatchNorm2d(32)
-        self.head = nn.Linear(448, 2)
+
+        u = 20
+
+        self.input = nn.Linear(4, u, bias=False)
+        self.FC1 = nn.Linear(u, u)
+        self.FC2 = nn.Linear(u, 2)
+
+        # self.conv1 = nn.Conv2d(1, 16, kernel_size=5, stride=2)
+        # self.bn1 = nn.BatchNorm2d(16)
+        # self.conv2 = nn.Conv2d(16, 32, kernel_size=5, stride=2)
+        # self.bn2 = nn.BatchNorm2d(32)
+        # self.conv3 = nn.Conv2d(32, 32, kernel_size=5, stride=2)
+        # self.bn3 = nn.BatchNorm2d(32)
+        # self.head = nn.Linear(448, 2)
 
     def forward(self, x):
-        x = F.relu(self.bn1(self.conv1(x)))
-        x = F.relu(self.bn2(self.conv2(x)))
-        x = F.relu(self.bn3(self.conv3(x)))
-        return self.head(x.view(x.size(0), -1))
+        x = F.relu(self.input(x))
+        x = F.relu(self.FC1(x))
+        x = self.FC2(x)
+
+        return x
+
+
+
+        # x = F.relu(self.bn1(self.conv1(x)))
+        # x = F.relu(self.bn2(self.conv2(x)))
+        # x = F.relu(self.bn3(self.conv3(x)))
+        # return self.head(x.view(x.size(0), -1))
 
 
 ######################################################################
@@ -262,12 +304,12 @@ def get_screen():
 
 
 env.reset()
-plt.figure()
+# plt.figure()
 # np.set_printoptions(threshold=np.nan)
 # print(get_screen().cpu().squeeze(0).squeeze(0).numpy())
-plt.imshow(get_screen().cpu().squeeze(0).squeeze(0).numpy(), interpolation='none')
-plt.title('Example extracted screen')
-plt.show()
+# plt.imshow(get_screen().cpu().squeeze(0).squeeze(0).numpy(), interpolation='none')
+# plt.title('Example extracted screen')
+# plt.show()
 
 ######################################################################
 # Training
@@ -291,19 +333,21 @@ plt.show()
 #    episode.
 #
 
-BATCH_SIZE = 128
+# BATCH_SIZE = 128
+BATCH_SIZE = 5
 GAMMA = 0.999
 EPS_START = 0.9
 EPS_END = 0.05
 EPS_DECAY = 200
-TARGET_UPDATE = 10
+TARGET_UPDATE = 1
 
 policy_net = DQN().to(device)
-target_net = DQN().to(device)
-target_net.load_state_dict(policy_net.state_dict())
-target_net.eval()
+# target_net = DQN().to(device)
+# target_net.load_state_dict(policy_net.state_dict())
+# target_net.eval()
 
-optimizer = optim.RMSprop(policy_net.parameters())
+optimizer = optim.Adam(policy_net.parameters(), lr=1e-6)
+# optimizer = optim.RMSprop(policy_net.parameters(), lr=1e-9)
 memory = ReplayMemory(10000)
 
 steps_done = 0
@@ -317,9 +361,19 @@ def select_action(state):
     steps_done += 1
     if sample > eps_threshold:
         with torch.no_grad():
-            return policy_net(state).max(1)[1].view(1, 1)
+            p = policy_net(state)
+            # print(p)
+            # print(p.max())
+            # print(p.max(0)[1])
+            # print(p.max(0).view(0, 0))
+            # return policy_net(state).max(1)[1].view(1, 1)
+            q = torch.tensor([[p.max(0)[1]]], device=device, dtype=torch.long)
+            # print('n', q.item())
+            return q
     else:
-        return torch.tensor([[random.randrange(2)]], device=device, dtype=torch.long)
+        q = torch.tensor([[random.randrange(2)]], device=device, dtype=torch.long)
+        # print('r', q.item())
+        return q
 
 
 episode_durations = []
@@ -339,7 +393,8 @@ def plot_durations():
         means = torch.cat((torch.zeros(99), means))
         plt.plot(means.numpy())
 
-    plt.pause(0.001)  # pause a bit so that plots are updated
+    if len(durations_t) % 10 == 0:
+        plt.pause(0.001)  # pause a bit so that plots are updated
     if is_ipython:
         display.clear_output(wait=True)
         display.display(plt.gcf())
@@ -370,34 +425,58 @@ def optimize_model():
     transitions = memory.sample(BATCH_SIZE)
     # Transpose the batch (see http://stackoverflow.com/a/19343/3343043 for
     # detailed explanation).
+
     batch = Transition(*zip(*transitions))
+
+    print(ep, '################################################################################')
+    print(batch)
 
     # Compute a mask of non-final states and concatenate the batch elements
     non_final_mask = torch.tensor(
         tuple(map(lambda s: s is not None, batch.next_state)), device=device, dtype=torch.uint8)
-    non_final_next_states = torch.cat([s for s in batch.next_state if s is not None])
-    state_batch = torch.cat(batch.state)
+
+    non_final_next_states = torch.cat([s.unsqueeze(0) for s in batch.next_state if s is not None])
+    state_batch = torch.cat([s.unsqueeze(0) for s in batch.state])
     action_batch = torch.cat(batch.action)
     reward_batch = torch.cat(batch.reward)
 
     # Compute Q(s_t, a) - the model computes Q(s_t), then we select the
     # columns of actions taken
     state_action_values = policy_net(state_batch).gather(1, action_batch)
+    # print('action_batch', action_batch)
+
+    # print('net_out', policy_net(state_batch))
+
+    # print('state_batch', state_batch)
+    # print('state_action_values', state_action_values)
 
     # Compute V(s_{t+1}) for all next states.
     next_state_values = torch.zeros(BATCH_SIZE, device=device)
-    next_state_values[non_final_mask] = target_net(non_final_next_states).max(1)[0].detach()
+    # next_state_values[non_final_mask] = target_net(non_final_next_states).max(1)[0].detach()
+    next_state_values[non_final_mask] = policy_net(non_final_next_states).max(1)[0].detach()
+
+    print(policy_net(non_final_next_states))
+    print(policy_net(non_final_next_states).max(1))
+    print('next state values', next_state_values)
+
     # Compute the expected Q values
-    expected_state_action_values = (next_state_values * GAMMA) + reward_batch
+    # expected_state_action_values = (next_state_values * GAMMA) + reward_batch
+    print(reward_batch)
+    expected_state_action_values = next_state_values * GAMMA + reward_batch
+
+    # print(expected_state_action_values)
 
     # Compute Huber loss
     loss = F.smooth_l1_loss(state_action_values, expected_state_action_values.unsqueeze(1))
 
+    print(loss.item())
+
     # Optimize the model
     optimizer.zero_grad()
     loss.backward()
-    for param in policy_net.parameters():
-        param.grad.data.clamp_(-1, 1)
+
+    # for param in policy_net.parameters():
+    #     param.grad.data.clamp_(-1, 1)
     optimizer.step()
 
 
@@ -413,26 +492,68 @@ def optimize_model():
 # the notebook and run lot more epsiodes.
 #
 
-num_episodes = 5000
+num_episodes = 500000
+# for i_episode in range(num_episodes):
+#     # Initialize the environment and state
+#     env.reset()
+#     last_screen = get_screen()
+#     current_screen = get_screen()
+#     state = current_screen - last_screen
+#     for t in count():
+#         # Select and perform an action
+#         action = select_action(state)
+#         _ , reward, done, _ = env.step(action.item())
+#         reward = torch.tensor([reward], device=device)
+# 
+#         # Observe new state
+#         last_screen = current_screen
+#         current_screen = get_screen()
+#         if not done:
+#             next_state = current_screen - last_screen
+#         else:
+#             next_state = None
+# 
+#         # Store the transition in memory
+#         memory.push(state, action, next_state, reward)
+# 
+#         # Move to the next state
+#         state = next_state
+# 
+#         # Perform one step of the optimization (on the target network)
+#         optimize_model()
+#         if done:
+#             episode_durations.append(t + 1)
+#             plot_durations()
+#             break
+#     # Update the target network
+#     if i_episode % TARGET_UPDATE == 0:
+#         target_net.load_state_dict(policy_net.state_dict())
+
+
 for i_episode in range(num_episodes):
+    ep = ep + 1
     # Initialize the environment and state
-    env.reset()
-    last_screen = get_screen()
-    current_screen = get_screen()
-    state = current_screen - last_screen
+    state = torch.from_numpy(env.reset()).to(device).float()
+    # last_screen = get_screen()
+    # current_screen = get_screen()
+    # state = current_screen - last_screen
     for t in count():
         # Select and perform an action
         action = select_action(state)
-        _, reward, done, _ = env.step(action.item())
+        next_state_, reward, done, _ = env.step(action.item())
+        next_state = torch.from_numpy(next_state_).to(device).float()
         reward = torch.tensor([reward], device=device)
 
+        if i_episode % 100 == 0:
+            get_screen()
+
         # Observe new state
-        last_screen = current_screen
-        current_screen = get_screen()
-        if not done:
-            next_state = current_screen - last_screen
-        else:
-            next_state = None
+        # last_screen = current_screen
+        # current_screen = get_screen()
+        # if not done:
+        #     next_state = current_screen - last_screen
+        # else:
+        #     next_state = None
 
         # Store the transition in memory
         memory.push(state, action, next_state, reward)
@@ -446,9 +567,12 @@ for i_episode in range(num_episodes):
             episode_durations.append(t + 1)
             plot_durations()
             break
-    # Update the target network
-    if i_episode % TARGET_UPDATE == 0:
-        target_net.load_state_dict(policy_net.state_dict())
+        # Update the target network
+        # if i_episode % TARGET_UPDATE == 0:
+        #     # print('UPDATE TARGET ################################################################################')
+        #     target_net.load_state_dict(policy_net.state_dict())
+
+
 
 print('Complete')
 env.render()
